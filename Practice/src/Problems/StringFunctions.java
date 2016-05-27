9e3b5c9b738b447a15c65fa0ba6691da57db169f
rushilanirudh <ranirudh@asu.edu>
2016-05-27 14:48:17 -0700

    first

---
+++ Practice/src/Problems/StringFunctions.java
@@ -0,0 +1,19 @@
+package Problems;
+
+public class StringFunctions {
+
+	public static int binarySearch(int[] arr,int k){
+		int[] arr_sort = arr.clone();
+		int N = arr_sort.length;
+		int idx = 0;
+		while(idx<N){
+			
+		}
+		return N;
+	}
+	public static void main(String[] args){
+		int[] a = {10,9,7,2,8};
+		int id = binarySearch(a,3);
+		
+	}
+}
